95e01fc4a90e5e4bba9054867716e14c8c02f11e
haruna <haruna0306@g.yju.ac.kr>
2026-04-18 21:43:48 +0900

    ToDoList

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { use, useState } from 'react';
+
+function App() {
+  // ToDoList変数
+  const [toDoList, setToDoList] = useState([]);
+  //　input変数
+  const [input, setInput] = useState('');
+    
+  //　入力をToDoListに追加する関数
+  const addToDo = () => {
+    if(input === ""){
+      alert("入力してください");
+      return;
+    }
+
+    // 入力をToDoListに追加
+    setToDoList([...toDoList, input]);
+    // 入力欄をリセット
+    setInput("");
+  }
+
+  //　入力値を管理して返還する関数
+  return(
+   
+   <div>
+      <h1>ToDo List</h1>
+      <input 
+      type="text"
+      value={input}
+      onChange={(e)=> setInput(e.target.value)}
+      placeholder='やることを入力してください'
+        />
+      <button onClick={addToDo}>追加</button>
+
+      <ul>
+        {toDoList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  
+  );
+
+
+}
+export default App;
+
